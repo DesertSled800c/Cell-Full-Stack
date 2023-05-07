300c7bf674f2dc6bffe58c7548ad2116e7d84146
Akshay Khona <desertsled800c@gmail.com>
2023-05-07 18:29:04 -0500

    API side completed

--- Cell/client/src/components/Game/Game.js
+++ Cell/client/src/components/Game/Game.js
@@ -4,9 +4,17 @@ import { Card } from "reactstrap";
 export default function Game({ game }) {
   return (
     <Card className="m-5 text-center" style={{ borderRadius: "20px" }}>
-        <h3>{game.title}</h3>
-        <h2>{game.body}</h2>
+      <h3>{game.title}</h3>
+      <h2>{game.body}</h2>
+      {game.tags && (
+        <h2>
+          <ul>
+            {game.tags.map((tag) => (
+              <li key={tag.id}>{tag.name}</li>
+            ))}
+          </ul>
+        </h2>
+      )}
     </Card>
   );
 }
-
